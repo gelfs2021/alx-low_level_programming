e0c8482c46ab08568186b19a0c13d886edf8fa2b
Gelfeto <gelfeto.gh@gmail.com>
2022-04-07 15:05:09 +0000

    the 7th

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main-the main function will print size of d/t data types
+*return: the fun will return 0 if it work correctly
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
