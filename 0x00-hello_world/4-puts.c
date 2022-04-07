222b17655f1106e1bcd647c992ae5f52dfc3e65c
Gelfeto <gelfeto.gh@gmail.com>
2022-04-07 14:11:20 +0000

    doing the 4th one

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main- the function will print Programming is like building  multilingual puzzl
+*Return: will return 0 if the function works correctly
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
