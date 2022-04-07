085db8cd893b836d460888850971cd3a110c23d6
Gelfeto <gelfeto.gh@gmail.com>
2022-04-07 13:56:20 +0000

    fixing betty error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void)
 
+/**
+ * main- this function prints "the phrase with proper grammar,
+ * but the outcome is a piece of art,"
+ * Return: if exited properly,
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
