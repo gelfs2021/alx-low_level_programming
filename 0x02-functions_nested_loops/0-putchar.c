6449eb8ccbe7a9941bbba56dba69791f774973bb
Gelfeto Gebre <gelfeto.gh@gmail.com>
2022-04-12 15:52:18 +0300

    the first work of the day

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar.c
+ *
+ * Return: it will return 0 if it works correctly
+ */
+int main()
+{
+	prints("0-putchar.c");
+	return 0;
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is the read me of 0x02-functions_nested_loops
