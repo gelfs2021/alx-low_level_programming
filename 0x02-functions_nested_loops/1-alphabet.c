470a80b0b52a21592417518cf66a8d078ce6b306
Gelfeto Gebre <gelfeto.gh@gmail.com>
2022-04-13 03:19:46 +0300

    doint the first 6 proj

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print from a to z 
+ *
+ * Return: void 
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+}
