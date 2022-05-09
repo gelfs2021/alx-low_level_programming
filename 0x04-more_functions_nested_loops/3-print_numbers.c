126df2d93a793ac2062f6a4c130b437a4ec1272b
Gelfeto <gelfeto.gh@gmail.com>
2022-05-09 09:26:10 +0000

    gelfs

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - it will prints 0 to 9
+*
+* Return: nothing
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+{
+	_putchar (i);
+}
+}
+
