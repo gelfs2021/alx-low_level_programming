d56c5262729c2437d8ab03ec7398ba0d212f33c8
Gelfeto <gelfeto.gh@gmail.com>
2022-05-10 07:15:04 +0000

    this

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints stight line
+ * @n: number of tims the _ prited
+ *
+ * Return: stright line`.
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	for (; i = n; i++)
+{
+	_putchar(95);
+}
+	putchar('\n');
+
+}
