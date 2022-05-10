f8ea4b2c21897d6e8d654ae2d26ccb84ea1c9909
Gelfeto <gelfeto.gh@gmail.com>
2022-05-10 07:07:32 +0000

    the hel

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j  = 0; j = 10; j++)
-{
-	for (i = 0; i <= 14; i++)
-{
-	_putchar ('i');
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n');
+	}
 }
-}
-	_putchar ('\n');
 
-}
