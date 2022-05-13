7390242ee0ae9ad544ad1537169fa9c708c88cff
Gelfeto <gelfeto.gh@gmail.com>
2022-05-13 14:04:00 +0000

    hall no

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,19 +13,19 @@ int i, j;
 
 if (size == 0)
 {
-_putchar ('\n');
+	_putchar ('\n');
 }
 else
 {
-for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 {
-	for (j = 0; j < size; j++)
+		for (j = 0; j < size; j++)
 {
-		_putchar(35);
+			_putchar(35);
 }
-if (i != (size - 1))
+	if (i < (size - 1))
 {
-	_putchar('\n');
+		_putchar('\n');
 }
 }
 }
