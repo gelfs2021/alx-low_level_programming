6cc5e5c2e19efa75bcab5dcd807c25a6795a753e
Gelfeto <gelfeto.gh@gmail.com>
2022-05-13 14:00:12 +0000

    hahs

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,7 +13,7 @@ int i, j;
 
 if (size == 0)
 {
-putchar ('\n');
+_putchar ('\n');
 }
 else
 {
@@ -21,11 +21,11 @@ for (i = 0; i < size; i++)
 {
 	for (j = 0; j < size; j++)
 {
-		putchar(35);
+		_putchar(35);
 }
 if (i != (size - 1))
 {
-	putchar('\n');
+	_putchar('\n');
 }
 }
 }
